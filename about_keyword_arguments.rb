89d56d0eadd30e8fc30fe3047a31b486d44272a2
Eliza Brock <eliza@elizabrocksoftware.com>
2015-04-09 15:31:09 -0500

    Second day of in-class q&a

--- about_keyword_arguments.rb
+++ about_keyword_arguments.rb
@@ -2,15 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutKeywordArguments < Neo::Koan
 
+  # corollary of: def method_with_keyword_arguments(one, two)
   def method_with_keyword_arguments(one: 1, two: 'two')
     [one, two]
   end
 
   def test_keyword_arguments
-    assert_equal __, method_with_keyword_arguments.class
-    assert_equal __, method_with_keyword_arguments
-    assert_equal __, method_with_keyword_arguments(one: 'one')
-    assert_equal __, method_with_keyword_arguments(two: 2)
+    assert_equal Array, method_with_keyword_arguments().class
+    assert_equal [1, 'two'], method_with_keyword_arguments()
+    assert_equal ['one', 'two'], method_with_keyword_arguments(one: 'one')
+    assert_equal [1, 2], method_with_keyword_arguments(two: 2)
   end
 
   def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
@@ -18,10 +19,10 @@ class AboutKeywordArguments < Neo::Koan
   end
 
   def test_keyword_arguments_with_wrong_number_of_arguments
-    exception = assert_raise (___) do
+    exception = assert_raise (ArgumentError) do
       method_with_keyword_arguments_with_mandatory_argument
     end
-    assert_match(/__/, exception.message)
+    assert_match(/0 for 1/, exception.message)
   end
 
   # THINK ABOUT IT:
