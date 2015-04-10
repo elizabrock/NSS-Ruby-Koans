9e6e9c63b60c8fa45506b49255dd9b1cc04e48da
Eliza Brock <eliza@elizabrocksoftware.com>
2015-04-10 10:07:11 -0500

    Day 3's in-class morning.

--- about_control_statements.rb
+++ about_control_statements.rb
@@ -3,12 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutControlStatements < Neo::Koan
 
   def test_if_then_else_statements
-    if true
+    value = true
+    if value
       result = :true_value
     else
       result = :false_value
     end
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_if_then_statements
@@ -16,7 +17,7 @@ class AboutControlStatements < Neo::Koan
     if true
       result = :true_value
     end
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_if_statements_return_values
@@ -25,14 +26,14 @@ class AboutControlStatements < Neo::Koan
             else
               :false_value
             end
-    assert_equal __, value
+    assert_equal :true_value, value
 
     value = if false
               :true_value
             else
               :false_value
             end
-    assert_equal __, value
+    assert_equal :false_value, value
 
     # NOTE: Actually, EVERY statement in Ruby will return a value, not
     # just if statements.
@@ -42,19 +43,19 @@ class AboutControlStatements < Neo::Koan
     value = if false
               :true_value
             end
-    assert_equal __, value
+    assert_equal nil, value
   end
 
   def test_condition_operators
-    assert_equal __, (true ? :true_value : :false_value)
-    assert_equal __, (false ? :true_value : :false_value)
+    assert_equal :true_value, (true ? :true_value : :false_value)
+    assert_equal :false_value, (false ? :true_value : :false_value)
   end
 
   def test_if_statement_modifiers
     result = :default_value
     result = :true_value if true
 
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_unless_statement
@@ -62,7 +63,7 @@ class AboutControlStatements < Neo::Koan
     unless false    # same as saying 'if !false', which evaluates as 'if true'
       result = :false_value
     end
-    assert_equal __, result
+    assert_equal :false_value, result
   end
 
   def test_unless_statement_evaluate_true
@@ -70,14 +71,14 @@ class AboutControlStatements < Neo::Koan
     unless true    # same as saying 'if !true', which evaluates as 'if false'
       result = :true_value
     end
-    assert_equal __, result
+    assert_equal :default_value, result
   end
 
   def test_unless_statement_modifier
     result = :default_value
     result = :false_value unless false
 
-    assert_equal __, result
+    assert_equal :false_value, result
   end
 
   def test_while_statement
@@ -87,7 +88,7 @@ class AboutControlStatements < Neo::Koan
       result = result * i
       i += 1
     end
-    assert_equal __, result
+    assert_equal 3628800, result
   end
 
   def test_break_statement
@@ -95,10 +96,14 @@ class AboutControlStatements < Neo::Koan
     result = 1
     while true
       break unless i <= 10
+      # break if i > 10
+      # if i > 10
+      #   break
+      # end
       result = result * i
       i += 1
     end
-    assert_equal __, result
+    assert_equal 3628800, result
   end
 
   def test_break_statement_returns_values
@@ -108,7 +113,7 @@ class AboutControlStatements < Neo::Koan
       i += 1
     end
 
-    assert_equal __, result
+    assert_equal 2, result
   end
 
   def test_next_statement
@@ -119,7 +124,7 @@ class AboutControlStatements < Neo::Koan
       next if (i % 2) == 0
       result << i
     end
-    assert_equal __, result
+    assert_equal [1, 3, 5, 7, 9], result
   end
 
   def test_for_statement
@@ -128,7 +133,7 @@ class AboutControlStatements < Neo::Koan
     for item in array
       result << item.upcase
     end
-    assert_equal [__, __, __], result
+    assert_equal ["FISH", "AND", "CHIPS"], result
   end
 
   def test_times_statement
@@ -136,7 +141,7 @@ class AboutControlStatements < Neo::Koan
     10.times do
       sum += 1
     end
-    assert_equal __, sum
+    assert_equal 10, sum
   end
 
 end
